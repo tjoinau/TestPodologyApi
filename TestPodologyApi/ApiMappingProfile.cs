e80966415e600e6692864456e3f2f7f789b7c8dd
tjo <tjo@LAPTOP-121>
2023-01-20 09:28:35 +0100

    try resolve gitignore

---
+++ TestPodologyApi/ApiMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TestPodologyModel.DTOs;
+using TestPodologyRepository.Entities;
+
+namespace TestPodologyApi
+{
+    public class ApiMappingProfile : Profile
+    {
+        public ApiMappingProfile()
+        {
+            CreateMap<Location, LocationDto>().ReverseMap();
+        }
+    }
+}
